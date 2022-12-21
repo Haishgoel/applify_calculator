dfa3bbbd554db3d7d9e4f1ef04af6408deb9ea42
harishgoel10 <harish.g@applify.co>
2022-12-21 12:46:00 +0530

    commenting each script

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var Sequelize = require("sequelize");
 var sequelize = require("../dbConnection").sequelize;
 module.exports = {
-
+ 
   plateform: require("./plateformModel")(Sequelize, sequelize, Sequelize.DataTypes),
   addcategory: require("./addcategory")(Sequelize, sequelize, Sequelize.DataTypes),
   appCalculate: require("./appCalculator")(Sequelize, sequelize, Sequelize.DataTypes),
